44bebefa5fb4366d8587d79b6f1c32e9d05cbd2f
Leanper <Leanper_A@163.com>
2019-04-17 17:47:47 +0800

    添加发布

--- pages/detail/detail.js
+++ pages/detail/detail.js
@@ -15,6 +15,7 @@ Page({
     swiperinfo:"",//tab 切换底部内容
     showOrHidden: false,//评论是否显示  false显示 true隐藏
     showOrHiddenInfo: true,//场馆介绍 景区须知是否显示 和showOrHidden相对应
+    publishInfo:"",//发布框内容
     goodArray: [
       { "title": "标题3", "src": "/images/img2.png", "name": "内容", "id": 23 },
       { "title": "brand", "src": '/images/img1.png', "name": "内容", "id": 3 },
@@ -82,10 +83,41 @@ Page({
       }
   },
 
+//获取发布框输入的内容
+  searchinfo:function(e){
+  
+  console.log("输入框内容：" + e.detail.value)
+  this.setData({
+    publishInfo: e.detail.value,
+  });
+  },
+
   //发布按钮点击
   onclickpublish:function(e){
 
+    console.log("====" + this.data.publishInfo)
+
+    //请求数据
+    wx.request({
+
+      url: 'https://api.apiopen.top/musicRanki=1'+this.data.publishInfo+'&user_id='+"1",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      success: function (res) {
+        console.log("====" + res)
+
+      },fail:function(res){
+        wx.showToast({
+          title: '发送失败',
+        })
+      }
+    })
+
+   
   },
+
+  //加载页面内容
   onLoad: function (option) {
     var that=this;
     console.log("接受的id:"+option.id)
